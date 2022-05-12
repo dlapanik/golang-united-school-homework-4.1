fbd24fa6e7a3d84b7ec77d6e8182cd34fab3c75b
Dzianis Lapanik <dzianis_lapanik@epam.com>
2022-05-12 18:29:25 +0300

    homework attempt #1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,16 @@
 package reverse_string
 
+import (
+	"strings"
+)
+
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	var b strings.Builder
+	runes := []rune(input)
+
+	for i := len(runes) - 1; i >= 0; i-- {
+		b.WriteRune(runes[i])
+	}
+
+	return b.String()
 }
